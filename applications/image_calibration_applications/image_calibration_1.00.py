f6d9462d3acd3a2825b04ff53507d674b9372de0
Rowan Twilley <twilley.rowan@gmail.com>
2023-06-23 12:03:31 +0200

    pure_grey_test_case_done

--- applications/image_calibration_applications/image_calibration_1.00.py
+++ applications/image_calibration_applications/image_calibration_1.00.py
@@ -9,25 +9,49 @@ per colomn processing method.
 
 import numpy as np
 from PIL import Image
+from tkinter import filedialog
+import tkinter as tk
 
-# Set the random seed
-np.random.seed(100)
+def image_selector():
+    root = tk.Tk()
+    root.withdraw()
+    root.focus_force()
+    root.attributes("-topmost",True)
+    image_path = filedialog.askopenfilename(filetypes=[("Image Files", "*.jpg;*.jpeg")])
+    
+    return image_path
 
-# Open the image file
-image = Image.open(r"processed_images\new_today.jpg")
+def save_image(modified_image): 
+    file_name = input("Enter the save file name :")
+    modified_image.save("C:\github\image_processing_python\processed_images(after correction)\\" + file_name + ".jpg")
+    
+def main(): 
+    
+    # Set the random seed
+    np.random.seed(100)
 
-if image.mode != 'RGB':
-    image = image.convert('RGB')
+    # Open the image file
+    selected_image = image_selector()
+    image = Image.open(selected_image)
 
-image_array = np.array(image)
+    if image.mode != 'RGB':
+        image = image.convert('RGB')
 
-num_columns = image_array.shape[1]
+    image_array = np.array(image)
 
-A = 1/(np.random.randn(num_columns)*(0.1) + 1)
-B = (-1)*np.random.randint(10,size=(num_columns))
+    num_columns = image_array.shape[1]
 
-for col in range(num_columns):
-    image_array[:, col, :] = A[col] * image_array[:, col, :] + B[col]
+    A = 1/(np.random.randn(num_columns)*(0.1) + 1)
+    B = (-1)*np.random.randint(10,size=(num_columns))
 
-modified_image = Image.fromarray(image_array)
-modified_image.show()
+    for col in range(num_columns):
+        image_array[:, col, :] = A[col] * image_array[:, col, :] + B[col]
+
+    modified_image = Image.fromarray(image_array)
+    modified_image.show()
+    
+    #save the image to the destination file location
+    save_image(modified_image)
+
+if __name__ == "__main__": 
+    main()
